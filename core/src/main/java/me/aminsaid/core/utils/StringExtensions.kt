7114667bd54d8e722f491d326563a29067c19db0
Amin Said <Amin.said@bosta.co>
2023-10-05 18:50:21 +0300

    fix StringExtensions.kt

--- core/src/main/java/me/aminsaid/core/utils/StringExtensions.kt
+++ core/src/main/java/me/aminsaid/core/utils/StringExtensions.kt
@@ -2,20 +2,25 @@ package me.aminsaid.core.utils
 
 import java.text.SimpleDateFormat
 import java.util.Locale
+import java.util.TimeZone
 
 // string
 fun String.getDayOfWeek(): String {
     val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.US)
+    inputFormat.timeZone = TimeZone.getTimeZone("UTC") // Set a specific timezone (e.g., UTC)
     val date = inputFormat.parse(this)
 
     val outputFormat = SimpleDateFormat("EEEE", Locale.US)
+    outputFormat.timeZone = TimeZone.getTimeZone("UTC") // Set the same timezone
     return outputFormat.format(date!!)
 }
 
 fun String.getDayOfWeekWithTime(): String {
     val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.US)
+    inputFormat.timeZone = TimeZone.getTimeZone("UTC") // Set a specific timezone (e.g., UTC)
     val date = inputFormat.parse(this)
 
     val outputFormat = SimpleDateFormat("EEEE, HH:mm a", Locale.US)
+    outputFormat.timeZone = TimeZone.getTimeZone("UTC") // Set the same timezone
     return outputFormat.format(date!!)
 }
--- core/src/test/java/me/aminsaid/core/StringExtensionsTest.kt
+++ core/src/test/java/me/aminsaid/core/StringExtensionsTest.kt
@@ -9,7 +9,7 @@ class StringExtensionsTest {
 
     @Test
     fun `getDayOfWeek should return correct day of the week`() {
-        val dateString = "2023-10-05T14:30:00-07:00" // Replace with your test date string
+        val dateString = "2023-10-05T14:30:00+00:00" // Replace with your test date string
         val expectedDayOfWeek = "Thursday"
 
         val result = dateString.getDayOfWeek()
@@ -19,7 +19,7 @@ class StringExtensionsTest {
 
     @Test
     fun `getDayOfWeekWithTime should return correct day of the week with time`() {
-        val dateString = "2023-10-05T14:30:00+02:00" // Replace with your test date string
+        val dateString = "2023-10-05T14:30:00+00:00" // Replace with your test date string
         val expectedDayOfWeekWithTime = "Thursday, 14:30 PM"
 
         val result = dateString.getDayOfWeekWithTime()
